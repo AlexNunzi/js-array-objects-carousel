f8b0bbcdb07709583824d50766741b9696eef37c
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-09 15:30:17 +0100

    Inizializzazione del progetto con il codice html/css/js che gestisce il carosello infinito preso dal progetto precedentemente realizzato. Inserimento delle immagini prendendo i path relativi dall'array fornito.

---
+++ js/main.js
@@ -0,0 +1,106 @@
+const imgArray = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const imgListDom = document.querySelector('.imgList');
+const miniaturesContainerDom = document.querySelector('.miniaturesContainer');
+let sliderImg = '';
+let miniatureImg = '';
+
+for (let i = 0; i < imgArray.length; i++) {
+    const newImgWrapper = document.createElement('div');
+    newImgWrapper.classList.add('imgWrapper');
+    newImgWrapper.innerHTML += `<img class="img" src="${imgArray[i].image}" alt="Wallpaper">`;
+
+
+    const newMiniature = document.createElement('div');
+    newMiniature.classList.add('miniature');
+    newMiniature.classList.add('overlay');
+    newMiniature.innerHTML += `<img src="${imgArray[i].image}" alt="Wallpaper">`
+
+    imgListDom.append(newImgWrapper);
+    miniaturesContainerDom.append(newMiniature);
+}
+
+
+
+const imgWrappersDom = document.getElementsByClassName('imgWrapper');
+const miniatureDom = document.getElementsByClassName('miniature');
+
+console.log(imgWrappersDom);
+console.log(miniatureDom);
+
+let selectedImg = 0;
+
+imgWrappersDom[selectedImg].classList.add('show');
+miniatureDom[selectedImg].classList.remove('overlay');
+miniatureDom[selectedImg].classList.add('selectedMiniature');
+
+
+const upDom = document.querySelector('#up');
+const downDom = document.querySelector('#down');
+
+downDom.addEventListener('click', function(){
+    if (selectedImg < imgWrappersDom.length - 1) {
+        imgWrappersDom[selectedImg].classList.remove('show');
+        miniatureDom[selectedImg].classList.add('overlay');
+        miniatureDom[selectedImg].classList.remove('selectedMiniature');
+        selectedImg++;
+        imgWrappersDom[selectedImg].classList.add('show');
+        miniatureDom[selectedImg].classList.remove('overlay');
+        miniatureDom[selectedImg].classList.add('selectedMiniature');
+        upDom.classList.remove('hide');
+
+    } else if (selectedImg == imgWrappersDom.length -1){
+        imgWrappersDom[selectedImg].classList.remove('show');
+        miniatureDom[selectedImg].classList.add('overlay');
+        miniatureDom[selectedImg].classList.remove('selectedMiniature');
+        selectedImg = 0;
+        imgWrappersDom[selectedImg].classList.add('show');
+        miniatureDom[selectedImg].classList.remove('overlay');
+        miniatureDom[selectedImg].classList.add('selectedMiniature');
+    }
+});
+
+
+upDom.addEventListener('click', function(){
+    if (selectedImg > 0) {
+        imgWrappersDom[selectedImg].classList.remove('show');
+        miniatureDom[selectedImg].classList.add('overlay');
+        miniatureDom[selectedImg].classList.remove('selectedMiniature');
+        selectedImg--;
+        imgWrappersDom[selectedImg].classList.add('show');
+        miniatureDom[selectedImg].classList.remove('overlay');
+        miniatureDom[selectedImg].classList.add('selectedMiniature');
+        downDom.classList.remove('hide');
+
+    } else if (selectedImg == 0) {
+        imgWrappersDom[selectedImg].classList.remove('show');
+        miniatureDom[selectedImg].classList.add('overlay');
+        miniatureDom[selectedImg].classList.remove('selectedMiniature');
+        selectedImg = (imgWrappersDom.length - 1);
+        imgWrappersDom[selectedImg].classList.add('show');
+        miniatureDom[selectedImg].classList.remove('overlay');
+        miniatureDom[selectedImg].classList.add('selectedMiniature');
+    }
+});
